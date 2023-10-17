823329904587542e0f507ee97c415f4b6caca1bc
Pavel <83354660+Pahapo@users.noreply.github.com>
2023-10-17 22:57:52 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ document.getElementById('create').addEventListener('click', function() {
 
     let random = (Math.random() + 1).toString(36).substring(2) + (Math.random() + 1).toString(36).substring(2);
 
-    voucher_pic.innerHTML = (random.substr(1, 16));;
+    voucher_pic.innerHTML = (random.substr(1, 6));;
     sum_pic.innerHTML = sum + "&#8381";
     count_pic.innerHTML = count;
 },false);
